3879d5777e686932f1bb2bc9c026447bb66fe809
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-05-12 20:07:13 -0300

    css iniciado

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
 const btn = document.querySelector("#btn");
+const span = document.querySelector(".span");
 const p = document.querySelector(".paragraph");
+
+function changeDOM(json) {
+  span.innerText = `ADVICE #${json.slip.id}`;
+  p.innerText = json.slip.advice;
+}
 
 async function getAdvice() {
   try {
     const response = await fetch("	https://api.adviceslip.com/advice");
     if (!response.ok) throw new Error();
     const json = await response.json();
-    p.innerText = json.slip.advice;
-    console.log(json.slip.advice);
+    changeDOM(json);
   } catch (erro) {
     console.log(erro);
   } finally {
