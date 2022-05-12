fb04badd53f16ad85b0c1420710036799c708ee6
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-05-12 15:32:50 -0300

    API aplicada

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const btn = document.querySelector("#btn");
+const p = document.querySelector(".paragraph");
+async function getAdvice() {
+  try {
+    const response = await fetch("	https://api.adviceslip.com/advice");
+    const json = await response.json();
+    p.innerText = json.slip.advice;
+    console.log(json.slip.advice);
+  } catch (erro) {
+    console.log(erro);
+  } finally {
+    console.log("fim");
+  }
+}
+
+btn.addEventListener("click", getAdvice);
