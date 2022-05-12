9dad3c0a1b22c1b281f342e242f9809825fa4b8f
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-05-12 15:49:46 -0300

    script atualizado

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 const btn = document.querySelector("#btn");
 const p = document.querySelector(".paragraph");
+
 async function getAdvice() {
   try {
     const response = await fetch("	https://api.adviceslip.com/advice");
+    if (!response.ok) throw new Error();
     const json = await response.json();
     p.innerText = json.slip.advice;
     console.log(json.slip.advice);
